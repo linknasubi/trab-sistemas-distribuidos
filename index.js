44c2a84a7b166f5dc77f29c5a8dc9053eadc2ebf
Gabriel Aragão <31046857+linknasubi@users.noreply.github.com>
2023-12-06 12:45:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const axios = require('axios');
-
 const { Blockchain } = require('./src/blockchain');
 const P2PServer = require('./src/network');
-
 
 const blockchain = new Blockchain();
 const p2pServer = new P2PServer(blockchain);
 
-
-function initNode() {
-    p2pServer.listen();
-    p2pServer.initHttpServer(); // Inicializa o servidor HTTP com uma porta dinâmica
-
+async function initNode() {
+    await p2pServer.start(); // Inicia o servidor P2P
 }
 
-initNode();
-
-
-async function desregistrarNo() {
-    try {
-        await axios.post('http://localhost:3000/unregister', {
-            address: `http://localhost:${p2pServer.port}`
-        });
-        console.log('Nó desregistrado com sucesso.');
-    } catch (error) {
-        console.error('Erro ao desregistrar o nó:', error);
-    }
-}
-
-process.on('SIGINT', () => {
-
-    desregistrarNo().then(() => {
-        process.exit(0);
-    });
-
-    console.log('Nó encerrando...');
-
+initNode().catch(error => {
+    console.error('Erro ao iniciar o nó:', error);
 });
+
+
